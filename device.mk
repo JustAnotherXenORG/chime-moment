9d21ddda65f89cc98b835f0108f5345d5a8b1792
dataoutputstream <mb150996@gmail.com>
2022-01-07 08:58:09 +0200

    chime: Build audio.primary.default
     * Keep quiet log
    
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: Ic89c682a962147068a734de49a62b89b7e96ea67

--- device.mk
+++ device.mk
@@ -65,6 +65,7 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.primary.bengal \
+    audio.primary.default \
     audio.usb.default \
     audio.usbv2.default \
     libaudio-resampler \
