4bba0c7f64529cac1b7169bd37c5012d10e60fc0
boedhack99 <boedhack@gmail.com>
2022-04-27 20:24:59 +0700

    chime: rootdir: Don't mount any fstab.* variant
    - Drop unused fstab.emmc
    
    Signed-off-by: boedhack99 <boedhack@gmail.com>
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: Ied220a59d1f98adc6ba4b79a9ebd4eabd65dd0a0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,7 +84,6 @@ BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=default
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 BOARD_KERNEL_BASE := 0x00000000
@@ -157,7 +156,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
 # Recovery
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_INCLUDE_RECOVERY_DTBO := true
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.emmc
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
