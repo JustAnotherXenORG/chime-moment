888b0646317fe640ec3e6129111d02e2266d78c3
frost. (frostg-012) <frost@nusantararom.org>
2022-07-28 15:07:52 +0700

    EvolutionX bringup!!
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: Ifc19b7bd9502c44cfd9bd6fdd28c61dbd04cb7dd

--- evolution_chime.mk
+++ evolution_chime.mk
@@ -11,16 +11,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from chime device
 $(call inherit-product, device/xiaomi/chime/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common EvolutionX stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+# Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
+# Quick Tap
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_chime
+PRODUCT_NAME := evolution_chime
 PRODUCT_DEVICE := chime
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 9T
+PRODUCT_MODEL := SM6115
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Maintainer name overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-evolution
