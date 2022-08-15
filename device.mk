dd802f4932adc5a76726d23ea7779f7c64d035a3
hpnightowl <iamhp2k@gmail.com>
2022-08-16 05:23:56 +0530

    chime: Drop audio.a2dp.default
    * Building from here:
      https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35
    
    Change-Id: Ib71c1abd381468ec3621d60e5ca959f738f51168
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>

--- device.mk
+++ device.mk
@@ -62,7 +62,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.3-impl \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.primary.bengal \
