eda5a24be686baadbecb923a22310deb71c206ec
frost. (frostg-012) <frost@nusantararom.org>
2022-08-16 05:24:24 +0700

    chime: releasetools: Correct message when patching image
     * Based on AOSP-ROM flashing message
    
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: If4ce9b8430d0988bb57295becb115333836ce0ad

--- releasetools.py
+++ releasetools.py
@@ -27,7 +27,7 @@ def AddImage(info, basename, dest):
   name = basename
   data = info.input_zip.read("IMAGES/" + basename)
   common.ZipWriteStr(info.output_zip, name, data)
-  info.script.Print("Patching %s" % name.replace(".img", ""))
+  info.script.Print("Patching %s image unconditionally..." % name.replace(".img", ""))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info):
